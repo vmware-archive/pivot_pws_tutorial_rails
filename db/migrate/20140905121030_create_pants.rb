dbe9aa6c801016986b1cb95a2bad3818dc198c0c
Andrew Bruce <andrew@camelpunch.com>
2014-09-05 16:41:51 +0100

    Revert "pants created"
    This reverts commit c5aff8d4be473f434319f5a2069aeff3fb0d4030.

--- db/migrate/20140905121030_create_pants.rb
+++
@@ -1,8 +0,0 @@
-class CreatePants < ActiveRecord::Migration
-  def change
-    create_table :pants do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905121030) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "pants", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
